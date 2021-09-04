ecc568e3c70519139f86b425338445901cb72df8
Randi Firmansyah <randykelvin26@gmail.com>
2021-09-04 22:59:13 +0700

    alhamdulillah beres semua

--- Areas/User/Controllers/HomeController.cs
+++ Areas/User/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace FutsalSemuaSenang.Areas.User.Controllers
@@ -124,9 +125,112 @@ namespace FutsalSemuaSenang.Areas.User.Controllers
             else if (Beta < 2) NamaLapangKosong = "Beta";
             else if (Charlie < 2) NamaLapangKosong = "Charlie";
 
+            //jam selesai
+            int jamSelesainya = Int16.Parse(data.JamMulai) + data.Durasi;
+
+            //harga
+            int harga = 100000 * data.Durasi;
+
             //disini push db
+            var booking = new Booking()
+            {
+                IdUser = Id,
+                NamaLapangan = NamaLapangKosong,
+                Tanggal = data.Tanggal,
+                JamMulai = data.JamMulai,
+                JamSelesai = jamSelesainya.ToString(),
+                Harga = harga,
+                Status = false,
+            };
+
+            _context.Add(booking);
+
+            _context.SaveChanges();
 
             //disini kirim email
+            Random nilai = new Random();
+            int KodeBooking = nilai.Next(10000000, 99999999);
+
+            //cari dulu usernya
+            var findUser = _context.User.Find(Id);
+
+            MailMessage email = new MailMessage();
+            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+            email.From = new MailAddress("futsalsemuasenang@gmail.com");
+            email.To.Add(findUser.Email);
+            email.Subject = "Order Booking Berhasil !";
+            email.Body =
+                "<h1 style='color:blue;'>Selamat, Order booking anda telah berhasil !\nTunggu email selanjutnya untuk konfirmasi dari kami.</h1><br><br>" +
+                "<table>" +
+                    "<tr>" +
+                        "<td>" +
+                            "Kode Booking" +
+                        "</td>" +
+                        "<td><h3>" +
+                            KodeBooking +
+                        "</h3></td>" +
+                    "<tr>" +
+                    "<tr>" +
+                        "<td>" +
+                            "Nama Pembooking" +
+                        "</td>" +
+                        "<td><h3>" +
+                            findUser.Name +
+                        "</h3></td>" +
+                    "<tr>" +
+                    "<tr>" +
+                        "<td>" +
+                            "Nama Lapangan" +
+                        "</td>" +
+                        "<td><h3>" +
+                            NamaLapangKosong +
+                        "</h3></td>" +
+                    "<tr>" +
+                    "<tr>" +
+                        "<td>" +
+                            "Jam Mulai" +
+                        "</h3>" +
+                        "<td><h3>" +
+                            data.JamMulai +
+                        "</h3></td>" +
+                    "<tr>" +
+                    "<tr>" +
+                        "<td>" +
+                            "Jam Selesai" +
+                        "</td>" +
+                        "<td><h3>" +
+                            jamSelesainya +
+                        "</h3></td>" +
+                    "<tr>" +
+                    "<tr>" +
+                        "<td>" +
+                            "Harga" +
+                        "</td>" +
+                        "<td><h3>" +
+                            harga +
+                        "</h3></td>" +
+                    "<tr>" +
+                    "<tr>" +
+                        "<td>" +
+                            "Status" +
+                        "</td>" +
+                        "<td>" +
+                            "<h2 style='color:blue;'>Berhasil</h2>" +
+                        "</td>" +
+                    "<tr>" +
+                "<table><br><br>" +
+                "<a href='mailto:futsalsemuasenang@gmail.com?subject=Bantuan&body=Halo'>Membutuhkan bantuan dari kami ?</a><br><br>" +
+                "<a href='https://localhost:5001'>Kunjungi website ?</a><br>";
+
+            email.IsBodyHtml = true;
+
+            SmtpServer.Port = 587;
+            SmtpServer.Credentials = new System.Net.NetworkCredential("futsalsemuasenang@gmail.com", "FutsalSemuaSenang!");
+            SmtpServer.EnableSsl = true;
+
+            SmtpServer.Send(email);
+
             ViewBag.Message = NamaLapangKosong;
             return View();
         }
